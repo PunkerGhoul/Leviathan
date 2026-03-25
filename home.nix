431ea8a07128084c6a3d944de00604c9cce9db26
PunkerGhoul <40646732+PunkerGhoul@users.noreply.github.com>
2026-03-25 01:10:52 -0500

    53

--- home.nix
+++ home.nix
@@ -22,11 +22,15 @@
   programs.git.enable = true;
 
   home.activation.setDefaultShell = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    current_shell="$(${pkgs.getent}/bin/getent passwd "$USER" | cut -d: -f7)"
+    current_shell="$(${pkgs.gnugrep}/bin/grep "^$USER:" /etc/passwd | ${pkgs.coreutils}/bin/cut -d: -f7)"
     target_shell="${pkgs.zsh}/bin/zsh"
 
     if [ "$current_shell" != "$target_shell" ]; then
       if [ -x /usr/bin/sudo ]; then
+        if ! ${pkgs.gnugrep}/bin/grep -qxF "$target_shell" /etc/shells; then
+          $DRY_RUN_CMD /usr/bin/sudo ${pkgs.coreutils}/bin/sh -c \
+            "${pkgs.coreutils}/bin/printf '%s\n' '$target_shell' >> /etc/shells"
+        fi
         $DRY_RUN_CMD /usr/bin/sudo chsh -s "$target_shell" "$USER"
       else
         echo "sudo is required to set zsh as the default shell for $USER" >&2
